0013da2f30d6770a2720eab347aac77594be7683
fabmax <fabmax.thiele@gmail.com>
2017-07-15 22:21:35 +0200

    Added a few unit tests (only math stuff so far)

---
+++ kool-core/src/test/kotlin/de/fabmax/cool/util/TestUtils.kt
@@ -0,0 +1,15 @@
+package de.fabmax.cool.util
+
+/**
+ * Test util functions
+ */
+
+/**
+ * Replacement for default assert() function which doesn't work if the corresponding JVM option isn't set (which
+ * is tedious in gradle)
+ */
+fun testAssert(check: Boolean, msg: String = "Assertion failed") {
+    if (!check) {
+        throw AssertionError(msg)
+    }
+}
