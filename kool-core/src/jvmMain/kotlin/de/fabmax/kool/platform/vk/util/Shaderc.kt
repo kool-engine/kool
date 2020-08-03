947e8abe984cfd218db61e2760218f62d6f252c3
fabmax <fabmax.thiele@gmail.com>
2020-08-03 20:14:17 +0200

    enable shaderc performance optimization

--- kool-core/src/jvmMain/kotlin/de/fabmax/kool/platform/vk/util/Shaderc.kt
+++ kool-core/src/jvmMain/kotlin/de/fabmax/kool/platform/vk/util/Shaderc.kt
@@ -1,26 +1,8 @@
 package de.fabmax.kool.platform.vk.util
 
-import de.fabmax.kool.util.PerfTimer
 import de.fabmax.kool.util.logE
 import org.lwjgl.util.shaderc.Shaderc
-import java.io.FileReader
 import java.nio.ByteBuffer
-
-fun main() {
-    val compiler = Shaderc.shaderc_compiler_initialize()
-
-    val srcTxt = FileReader("shaders/shader.vert").readText()
-
-    val perf = PerfTimer()
-    val result = Shaderc.shaderc_compile_into_spv(compiler, srcTxt, Shaderc.shaderc_vertex_shader, "shader.vert", "main", 0L)
-    val status = Shaderc.shaderc_result_get_compilation_status(result)
-    println("compilation finished in ${perf.takeSecs()} s, status: $status")
-
-    val buf = Shaderc.shaderc_result_get_bytes(result)!!
-    println("result size: ${buf.remaining()}")
-
-    Shaderc.shaderc_compiler_release(compiler)
-}
 
 object Shaderc {
     private val compiler = Shaderc.shaderc_compiler_initialize()
@@ -32,13 +14,17 @@ object Shaderc {
         compileShader(src, Shaderc.shaderc_fragment_shader, name, entryPoint)
 
     private fun compileShader(src: String, shaderKind: Int, fName: String, entryPoint: String): CompileResult {
-        val result = Shaderc.shaderc_compile_into_spv(compiler, src, shaderKind, fName, entryPoint, 0L)
+        val options = Shaderc.shaderc_compile_options_initialize()
+        Shaderc.shaderc_compile_options_set_optimization_level(options, Shaderc.shaderc_optimization_level_performance)
+        Shaderc.shaderc_compile_options_set_target_env(options, Shaderc.shaderc_target_env_webgpu, 0)
 
+        val result = Shaderc.shaderc_compile_into_spv(compiler, src, shaderKind, fName, entryPoint, options)
         val status = Shaderc.shaderc_result_get_compilation_status(result)
         if (status != 0) {
             logE { "Failed to compile shader $fName ($status):\n${Shaderc.shaderc_result_get_error_message(result)}" }
         }
 
+        Shaderc.shaderc_compile_options_release(options)
         return CompileResult(result)
     }
 
