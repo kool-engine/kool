abe83b1022cccba1404e3f7e92e3905db41a091b
Andrey Zakharov <andrey.al.zakharov@gmail.com>
2022-06-01 14:45:22 +0300

    Draft for chain roller TBD meshInstancing TBD PhysX tweaks

--- kool-demo/src/commonMain/kotlin/de/fabmax/kool/demo/physics/joints/PendulumDemo.kt
+++ kool-demo/src/commonMain/kotlin/de/fabmax/kool/demo/physics/joints/PendulumDemo.kt
@@ -7,9 +7,7 @@ import de.fabmax.kool.demo.Demo
 import de.fabmax.kool.demo.DemoScene
 import de.fabmax.kool.math.*
 import de.fabmax.kool.physics.*
-import de.fabmax.kool.physics.geometry.BoxGeometry
-import de.fabmax.kool.physics.geometry.PlaneGeometry
-import de.fabmax.kool.physics.geometry.SphereGeometry
+import de.fabmax.kool.physics.geometry.*
 import de.fabmax.kool.physics.joints.*
 import de.fabmax.kool.pipeline.Texture2d
 import de.fabmax.kool.pipeline.shading.pbrShader
@@ -17,6 +15,7 @@ import de.fabmax.kool.scene.*
 import de.fabmax.kool.util.Color
 import de.fabmax.kool.util.MdColor
 import kotlin.math.PI
+import kotlin.math.ceil
 
 class PendulumDemo : DemoScene("Physics - Pendulum") {
     private val physicsWorld by lazy { PhysicsWorld().also {
@@ -54,7 +53,10 @@ class PendulumDemo : DemoScene("Physics - Pendulum") {
 
     private val pendulums = mutableListOf<Pendulum>()
     private val springs by lazy {
-        Springs(world = physicsWorld)
+        Springs(world = physicsWorld, origin = Vec3f(-20f, 0f, -20f))
+    }
+    private val chainRoller by lazy {
+        ChainRoller(origin = Vec3f(20f, 10f, -20f))
     }
 
     override fun Scene.setupMainScene(ctx: KoolContext) {
@@ -157,6 +159,9 @@ class PendulumDemo : DemoScene("Physics - Pendulum") {
 
         mainScene += springs
         springs.start()
+
+        mainScene += chainRoller
+        chainRoller.start(physicsWorld)
     }
 
     override fun dispose(ctx: KoolContext) {
@@ -210,11 +215,6 @@ class PendulumDemo : DemoScene("Physics - Pendulum") {
             MdColor.LIGHT_GREEN, MdColor.LIME, MdColor.YELLOW, MdColor.AMBER, MdColor.ORANGE, MdColor.DEEP_ORANGE))
             .apply { index = 1 }
 
-        // chaingenerator -
-        // interface ChainGenerator {
-        // firstNode
-        // on edge
-        // on node
         init {
             actors.add(pin)
             +pin.toMesh(Color.WHITE)
@@ -239,6 +239,8 @@ class PendulumDemo : DemoScene("Physics - Pendulum") {
                 joints.add(makeEdge(prevNode, pong))
                 prevNode = pong
             }
+
+            children.last().addDebugAxis()
         }
 
         fun start() = expose()
@@ -260,6 +262,7 @@ class PendulumDemo : DemoScene("Physics - Pendulum") {
     }
 
     class Springs(val world: PhysicsWorld,
+                  private val origin: Vec3f = Vec3f(0f, 0f, 0f),
                   private val dy: Float = 1.5f,
                   private val totalHeight: Float = 20f,
                   private val pinHeight: Float = 5f,
@@ -276,7 +279,7 @@ class PendulumDemo : DemoScene("Physics - Pendulum") {
 
                 for ( dm in 0 until n ) {
                     val realDamp = dm / n.toFloat()
-                    val initPos = Mat4f().translate(- dy * (st+1), totalHeight, - dy * (dm + 1))
+                    val initPos = Mat4f().translate(origin).translate(- dy * (st+1), totalHeight, - dy * (dm + 1))
                     val basement = RigidStatic(initPos.rotate(90f, Vec3f.X_AXIS)).apply {
                         attachShape(Shape(BoxGeometry(Vec3f(0.5f, 0.5f, 0.2f)), bodyMaterial))
                     }
@@ -313,17 +316,149 @@ class PendulumDemo : DemoScene("Physics - Pendulum") {
         }
 
     }
+
+    class ChainRoller(val origin: Vec3f = Vec3f(10f, 10f, -10f),
+                      val shaftRadius: Float = 1f,
+                      val shaftWidth: Float = 5f,
+                      val n: Int = 20, // between end and start of semi-circled parts
+                      val chainSegmentLength: Float = 1f,         // 1 chain segment length
+                      val chainSegmentSize: Float = 0.5f,
+                      val chainMass: Float = 10f,
+    ): Group() {
+        val pin = RigidStatic(Mat4f().translate(origin)).apply {
+            attachShape(Shape(BoxGeometry(Vec3f(0.3f))))
+        }
+
+        val disk = RigidDynamic(1f).apply {
+            // TBD rework
+            attachShape(Shape(
+                geometry = CylinderGeometry(shaftWidth, shaftRadius),
+                localPose = Mat4f().rotate(90f, Vec3f.Y_AXIS)
+            ))
+            attachShape(Shape(
+                geometry = CylinderGeometry(1f, shaftRadius + chainSegmentSize * 5),
+                localPose = Mat4f().translate(0f, 0f, shaftWidth/2f).rotate(90f, Vec3f.Y_AXIS)
+            ))
+            attachShape(Shape(
+                geometry = CylinderGeometry(1f, shaftRadius + chainSegmentSize * 5),
+                localPose = Mat4f().translate(0f, 0f, -shaftWidth/2f).rotate(-90f, Vec3f.Y_AXIS)
+            ))
+        }
+
+        private var diskJoint: Joint? = RevoluteJoint(pin, disk,
+            Mat4f().rotate(90f, Vec3f.Y_AXIS),
+            Mat4f().rotate(-90f, Vec3f.Y_AXIS)).apply {
+            enableAngularMotor(1f, 100f)
+        }
+
+        val chains = mutableListOf<RigidActor>()
+        val joints = mutableListOf<Joint>()
+
+        lateinit var world: PhysicsWorld
+
+        init {
+            + pin.toMesh(Color.RED)
+            + disk.toMesh(Color.GREEN)
+
+            // logo time
+            val halfCircleChains = ceil(PI * shaftRadius / chainSegmentLength).toInt()
+            val angleStep = (PI / halfCircleChains.toFloat()).toDeg().toFloat()
+            if ( halfCircleChains < 3 ) {
+                println("warning shaftRadius too small $shaftRadius")
+            }
+
+            for ( c in 0 .. halfCircleChains ) {
+                val r = MutableVec3f( shaftRadius + chainSegmentSize, 0f, 0f)
+                r.rotate(c * angleStep, Vec3f.Z_AXIS, r)
+                chains.add(RigidDynamic(chainMass, Mat4f().translate(origin).translate(r)).apply {
+                    attachShape(Shape(SphereGeometry(chainSegmentSize)))
+                })
+            }
+
+            for ( c in 1 until n ) {
+                val pos = Mat4f().translate(origin).translate( -shaftRadius - chainSegmentSize, -c * chainSegmentLength, 0f)
+                chains.add(RigidDynamic(chainMass, pos).apply {
+                    attachShape(Shape(SphereGeometry(chainSegmentSize)))
+                })
+            }
+
+            for ( c in 0 .. halfCircleChains ) {
+                val r = MutableVec3f( -shaftRadius - chainSegmentSize, 0f, 0f)
+                r.rotate(c * angleStep, Vec3f.Z_AXIS, r)
+                val pos = Mat4f().translate(origin).translate(0f, -n * chainSegmentLength, 0f).translate(r)
+                chains.add(RigidDynamic(chainMass, pos).apply {
+                    attachShape(Shape(SphereGeometry(chainSegmentSize)))
+                })
+            }
+
+            for ( c in 1 until n ) {
+                val pos = Mat4f().translate(origin).translate(shaftRadius + chainSegmentSize, -c * chainSegmentLength, 0f)
+                chains.add(RigidDynamic(chainMass, pos).apply {
+                    attachShape(Shape(SphereGeometry(chainSegmentSize)))
+                })
+            }
+
+            chains.fold(chains.first()) { prev, current ->
+                joints.add(DistanceJoint(prev, current, Mat4f(), Mat4f()).apply {
+                    setMaxDistance(chainSegmentLength)
+                    setMinDistance(chainSegmentLength)
+                })
+                current
+            }
+            joints.add(DistanceJoint(chains.first(), chains.last(), Mat4f(), Mat4f()).apply {
+                setMaxDistance(chainSegmentLength)
+                setMinDistance(chainSegmentLength)
+            })
+
+            chains.forEach {
+                +it.toMesh(Color.LIGHT_YELLOW).also {
+                    it.addDebugAxis()
+                }
+            }
+        }
+
+        fun start(world: PhysicsWorld) {
+            this.world = world
+            world.addActor(pin)
+            world.addActor(disk)
+            chains.forEach {
+                world.addActor(it)
+            }
+        }
+
+        override fun dispose(ctx: KoolContext) {
+            if ( !::world.isInitialized ) return
+
+            world.removeActor(pin)
+            world.removeActor(disk)
+            chains.forEach { world.removeActor(it) }
+            chains.clear()
+            joints.forEach { it.dispose(ctx) }
+            joints.clear()
+        }
+    }
 }
 
+fun Node.parentGroup(): Group {
+    return when {
+        this is Group -> this
+        this.parent != null -> this.parent!!.parentGroup()
+        else -> throw RuntimeException("no parent group $this")
+    }
+}
 
-fun Group.addDebugAxis() {
-//    val x = lineMesh("x") { addLine(Vec3f.ZERO, Vec3f(1f, 0f, 0f), Color.RED) }
-//    +x
-//    +lineMesh("y") { addLine(Vec3f.ZERO, Vec3f(0f, 1f, 0f), Color.GREEN) }
-//    +lineMesh("z") { addLine(Vec3f.ZERO, Vec3f(0f, 0f, 1f), Color.BLUE) }
-//    onUpdate += {
-//        x.transform.set(this@CommonRigidActor.transform)
-//            this@group.setDirty()
-//
-//    }
+fun Node.addDebugAxis() {
+    with(parentGroup()) {
+        val x = lineMesh("x") { addLine(Vec3f.ZERO, Vec3f(1f, 0f, 0f), Color.RED) }
+        val y = lineMesh("y") { addLine(Vec3f.ZERO, Vec3f(0f, 1f, 0f), Color.GREEN) }
+        val z = lineMesh("z") { addLine(Vec3f.ZERO, Vec3f(0f, 0f, 1f), Color.BLUE) }
+        +x
+        +y
+        +z
+        onUpdate += {
+            x.modelMat.set(this@addDebugAxis.modelMat)
+            y.modelMat.set(this@addDebugAxis.modelMat)
+            z.modelMat.set(this@addDebugAxis.modelMat)
+        }
+    }
 }
