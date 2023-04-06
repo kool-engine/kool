eba471948b3d4e54af23fbb81ae349063e3d62f1
B4rtware <34386047+B4rtware@users.noreply.github.com>
2023-04-06 21:32:37 +0200

    refactor: allow null as initial value for remember and add onChange parameter

--- kool-core/src/commonMain/kotlin/de/fabmax/kool/modules/ui2/UiScope.kt
+++ kool-core/src/commonMain/kotlin/de/fabmax/kool/modules/ui2/UiScope.kt
@@ -52,7 +52,8 @@ inline fun UiScope.Popup(
 }
 
 inline fun <reified T: Any> UiScope.remember(provider: () -> T): T = uiNode.weakMemory.weakMemory(provider)
-fun <T: Any> UiScope.remember(initialState: T): MutableStateValue<T> = uiNode.weakMemory.weakMemory { mutableStateOf(initialState) }
+fun <T: Any?> UiScope.remember(initialState: T): MutableStateValue<T> = uiNode.weakMemory.weakMemory { mutableStateOf(initialState) }
+fun <T: Any?> UiScope.remember(initialState: T, onChange: (T) -> Unit) = uiNode.weakMemory.weakMemory { mutableStateOf(initialState).onChange(onChange) }
 fun UiScope.rememberScrollState(): ScrollState = uiNode.weakMemory.weakMemory { ScrollState() }
 fun UiScope.rememberListState(): LazyListState = uiNode.weakMemory.weakMemory { LazyListState() }
 fun UiScope.clearMemory() = uiNode.weakMemory.clear()
