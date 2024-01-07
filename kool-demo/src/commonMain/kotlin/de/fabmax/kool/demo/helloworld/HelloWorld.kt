78a9d4c87d89d674307adcdbdb069cd642be8d25
fabmax <fabmax.thiele@gmail.com>
2024-01-07 20:54:21 +0100

    restore correct hello world demo

--- kool-demo/src/commonMain/kotlin/de/fabmax/kool/demo/helloworld/HelloWorld.kt
+++ kool-demo/src/commonMain/kotlin/de/fabmax/kool/demo/helloworld/HelloWorld.kt
@@ -2,15 +2,14 @@ package de.fabmax.kool.demo.helloworld
 
 import de.fabmax.kool.KoolContext
 import de.fabmax.kool.demo.DemoScene
-import de.fabmax.kool.modules.ksl.KslShader
-import de.fabmax.kool.modules.ksl.blocks.mvpMatrix
-import de.fabmax.kool.modules.ksl.lang.times
-import de.fabmax.kool.pipeline.Attribute
-import de.fabmax.kool.pipeline.vertexAttribFloat3
-import de.fabmax.kool.pipeline.vertexAttribFloat4
+import de.fabmax.kool.math.Vec3f
+import de.fabmax.kool.math.deg
+import de.fabmax.kool.modules.ksl.KslPbrShader
 import de.fabmax.kool.scene.Scene
 import de.fabmax.kool.scene.addColorMesh
 import de.fabmax.kool.scene.defaultOrbitCamera
+import de.fabmax.kool.util.Color
+import de.fabmax.kool.util.Time
 
 class HelloWorld : DemoScene("Hello World") {
     override fun Scene.setupMainScene(ctx: KoolContext) {
@@ -22,23 +21,19 @@ class HelloWorld : DemoScene("Hello World") {
                     colored()
                 }
             }
-            shader = KslShader("Hello world shader") {
-                val interStageColor = interStageFloat4()
-
-                vertexStage {
-                    main {
-                        val mvp = mvpMatrix()
-                        val localPosition = float3Var(vertexAttribFloat3(Attribute.POSITIONS))
-                        outPosition set mvp.matrix * float4Value(localPosition, 1f.const)
-                        interStageColor.input set vertexAttribFloat4(Attribute.COLORS)
-                    }
-                }
-                fragmentStage {
-                    main {
-                        colorOutput(interStageColor.output)
-                    }
-                }
+            shader = KslPbrShader {
+                color { vertexColor() }
+                metallic(0f)
+                roughness(0.25f)
             }
+            onUpdate {
+                transform.rotate(45f.deg * Time.deltaT, Vec3f.X_AXIS)
+            }
+        }
+
+        lighting.singleDirectionalLight {
+            setup(Vec3f(-1f, -1f, -1f))
+            setColor(Color.WHITE, 5f)
         }
     }
 }
