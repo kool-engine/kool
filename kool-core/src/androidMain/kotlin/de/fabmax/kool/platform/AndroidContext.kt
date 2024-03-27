d29f09a6d11931eb05ce855fc710bcb64f0c9d22
fabmax <fabmax.thiele@gmail.com>
2024-03-27 21:02:54 +0100

    forward log messages to logcat

--- kool-core/src/androidMain/kotlin/de/fabmax/kool/platform/AndroidContext.kt
+++ kool-core/src/androidMain/kotlin/de/fabmax/kool/platform/AndroidContext.kt
@@ -3,8 +3,12 @@ package de.fabmax.kool.platform
 import de.fabmax.kool.KoolContext
 import de.fabmax.kool.KoolSystem
 import de.fabmax.kool.pipeline.backend.gl.RenderBackendGlImpl
+import de.fabmax.kool.util.Log
 import de.fabmax.kool.util.RenderLoopCoroutineDispatcher
+import de.fabmax.kool.util.Time
 import de.fabmax.kool.util.logE
+
+typealias AndroidLog = android.util.Log
 
 class AndroidContext : KoolContext() {
     override val backend: RenderBackendGlImpl
@@ -56,5 +60,25 @@ class AndroidContext : KoolContext() {
 
         // execute draw queues
         backend.renderFrame(this)
+    }
+
+    companion object {
+        init {
+            if (Log.printer == Log.DEFAULT_PRINTER) {
+                Log.printer = { lvl, tag, message ->
+                    val ctx = KoolSystem.getContextOrNull()
+                    val frmTxt = ctx?.let { "f:${Time.frameCount}  " } ?: ""
+                    val txt = "$frmTxt$message"
+                    when (lvl) {
+                        Log.Level.TRACE -> AndroidLog.d(tag, "TRC: $txt")
+                        Log.Level.DEBUG -> AndroidLog.d(tag, txt)
+                        Log.Level.INFO -> AndroidLog.i(tag, txt)
+                        Log.Level.WARN -> AndroidLog.w(tag, txt)
+                        Log.Level.ERROR -> AndroidLog.e(tag, txt)
+                        Log.Level.OFF -> { }
+                    }
+                }
+            }
+        }
     }
 }
