bc52d323b490ebd8e3c31aecc0c13f3246bb25a9
fabmax <fabmax.thiele@gmail.com>
2024-06-20 23:01:43 +0200

    fixed undo/redo remove component

--- kool-editor/src/commonMain/kotlin/de/fabmax/kool/editor/actions/RemoveComponentAction.kt
+++ kool-editor/src/commonMain/kotlin/de/fabmax/kool/editor/actions/RemoveComponentAction.kt
@@ -1,28 +1,33 @@
 package de.fabmax.kool.editor.actions
 
 import de.fabmax.kool.editor.api.GameEntity
-import de.fabmax.kool.editor.components.GameEntityComponent
+import de.fabmax.kool.editor.components.GameEntityDataComponent
 import de.fabmax.kool.editor.data.EntityId
 import de.fabmax.kool.editor.util.gameEntity
 import de.fabmax.kool.util.launchOnMainThread
 
 class RemoveComponentAction(
     val entityId: EntityId,
-    val component: GameEntityComponent
+    component: GameEntityDataComponent<*>
 ) : EditorAction {
 
+    private val componentType = component.componentType
+    private val componentInfo = component.componentInfo
     private val gameEntity: GameEntity? get() = entityId.gameEntity
 
-    // fixme: component is not recreated on undo / redo, therefore redo can fail
-
     override fun doAction() {
-        gameEntity?.removeComponent(component)
-        refreshComponentViews()
+        val entity = gameEntity ?: return
+        entity.components.firstOrNull { it.componentType == componentType }?.let { rem ->
+            entity.removeComponent(rem)
+            refreshComponentViews()
+        }
     }
 
     override fun undoAction() {
+        val entity = gameEntity ?: return
         launchOnMainThread {
-            gameEntity?.addComponentLifecycleAware(component)
+            val component = entity.createDataComponent(componentInfo)
+            entity.addComponentLifecycleAware(component)
             refreshComponentViews()
         }
     }
