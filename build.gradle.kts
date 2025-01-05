967a41ad75123c054e17c8b29f118109d06a3479
fabmax <fabmax.thiele@gmail.com>
2025-01-05 17:33:20 +0100

    v0.16.0 release

--- build.gradle.kts
+++ build.gradle.kts
@@ -8,7 +8,7 @@ plugins {
 
 allprojects {
     group = "de.fabmax.kool"
-    version = "0.16.0-SNAPSHOT"
+    version = "0.16.0"
 
     repositories {
         mavenLocal()
--- kool-core/src/commonMain/kotlin/de/fabmax/kool/KoolContext.kt
+++ kool-core/src/commonMain/kotlin/de/fabmax/kool/KoolContext.kt
@@ -140,6 +140,6 @@ abstract class KoolContext {
 
     companion object {
         // automatically updated by gradle script on build
-        const val KOOL_VERSION = "0.16.0-SNAPSHOT"
+        const val KOOL_VERSION = "0.16.0"
     }
 }
