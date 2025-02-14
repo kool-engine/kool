4fb6cd80f4f65846167667f8d2a02a71fae398cc
fabmax <fabmax.thiele@gmail.com>
2025-02-14 19:12:34 +0100

    fixed android build

--- kool-core/src/androidMain/kotlin/de/fabmax/kool/modules/filesystem/FileSystemAssetLoaderAndroid.kt
+++ kool-core/src/androidMain/kotlin/de/fabmax/kool/modules/filesystem/FileSystemAssetLoaderAndroid.kt
@@ -7,7 +7,7 @@ import java.io.ByteArrayInputStream
 
 class FileSystemAssetLoaderAndroid(baseDir: FileSystemDirectory): FileSystemAssetLoader(baseDir) {
     override suspend fun loadImageAtlas(ref: AssetRef.ImageAtlas): LoadedAsset.ImageAtlas {
-        val refCopy = AssetRef.BufferedImage2d(ref.path, ref.props)
+        val refCopy = AssetRef.BufferedImage2d(ref.path, ref.format, ref.resolveSize)
         val result = loadBufferedImage2d(refCopy).result.mapCatching {
             imageAtlasTextureData(it, ref.tilesX, ref.tilesY)
         }
@@ -18,7 +18,7 @@ class FileSystemAssetLoaderAndroid(baseDir: FileSystemDirectory): FileSystemAsse
         val tex = loadData(ref.path)
         val result = tex.mapCatching { buf ->
             ByteArrayInputStream(buf.toArray()).use {
-                PlatformAssetsImpl.readImageData(it, MimeType.forFileName(ref.path), ref.props)
+                PlatformAssetsImpl.readImageData(it, MimeType.forFileName(ref.path), ref.format, ref.resolveSize)
             }
         }
         return LoadedAsset.BufferedImage2d(ref, result)
