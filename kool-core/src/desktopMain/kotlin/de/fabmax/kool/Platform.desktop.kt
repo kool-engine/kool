529915db0d1bdc5c6f1b0f95ddf3cc8739314dd1
Max Thiele <fabmax.thiele@gmail.com>
2025-05-07 21:43:41 +0200

    changed log output format

--- kool-core/src/desktopMain/kotlin/de/fabmax/kool/Platform.desktop.kt
+++ kool-core/src/desktopMain/kotlin/de/fabmax/kool/Platform.desktop.kt
@@ -6,7 +6,6 @@ import de.fabmax.kool.platform.MonitorSpec
 import de.fabmax.kool.util.*
 import org.lwjgl.glfw.GLFW
 import org.lwjgl.glfw.GLFWErrorCallback
-import java.text.SimpleDateFormat
 import java.util.*
 
 actual fun Double.toString(precision: Int): String = "%.${precision.clamp(0, 12)}f".format(Locale.ENGLISH, this)
@@ -75,7 +74,7 @@ internal object DesktopImpl {
 }
 
 private object DesktopLogPrinter : LogPrinter {
-    val dateFmt = SimpleDateFormat("HH:mm:ss.SSS", Locale.US)
+    val startTime = System.currentTimeMillis()
 
     val numberRegex = """[0-9a-fA-F]+""".toRegex()
 
@@ -83,54 +82,57 @@ private object DesktopLogPrinter : LogPrinter {
     var prevMsg: String? = null
 
     override fun print(lvl: Log.Level, tag: String?, message: String) {
-        synchronized(dateFmt) {
-            val timestamp = coloredText(dateFmt.format(System.currentTimeMillis()), MdColor.BROWN tone 300)
-            val frameFmt = if (Time.frameCount > 999) "…%03d".format(Time.frameCount % 1000) else "%4d".format(Time.frameCount)
-            val frame = coloredText("f:$frameFmt", MdColor.PURPLE tone 300)
+        val timestamp = coloredText("%8.3f s".format((System.currentTimeMillis() - startTime) / 1000.0), MdColor.BROWN tone 300)
+        val frameFmt = if (Time.frameCount > 999) "…%03d".format(Time.frameCount % 1000) else "%4d".format(Time.frameCount)
+        val frame = coloredText("f:$frameFmt", MdColor.PURPLE tone 300)
 
-            val style = styles[lvl.level]
-            val tagStr = coloredText(" ${lvl.indicator} ", Color.WHITE, bgColor = style.bgColor, bold = true) +
-                    (tag?.let { coloredText(" $it:", style.fgColor, bold = style.bold) } ?: " ")
+        val style = styles[lvl.level]
+        val tagStr = coloredText(" ${lvl.indicator} ", Color.WHITE, bgColor = style.bgColor, bold = true) +
+                (tag?.let { coloredText(" $it:", style.fgColor, bold = style.bold) } ?: " ")
 
-            val txt = message.replace("\n", "\n  ")
-            val messageStr = when (lvl) {
-                Log.Level.WARN -> coloredText(txt, MdColor.AMBER tone 200)
-                Log.Level.ERROR -> coloredText(txt, MdColor.RED tone 200)
-                else -> txt
+        val txt = message.replace("\n", "\n  ")
+        val messageStr = when (lvl) {
+            Log.Level.WARN -> coloredText(txt, MdColor.AMBER tone 200)
+            Log.Level.ERROR -> coloredText(txt, MdColor.RED tone 200)
+            else -> txt
+        }
+
+        val isVkMsg = "VkValidation" in (tag ?: "")
+        val printMsg = "$tagStr $messageStr"
+        val checkMsg = if (isVkMsg) printMsg.replace(numberRegex) { "" } else printMsg
+        if (checkMsg == prevMsg) {
+            sameMsgCnt++
+            val colored = coloredText("<Repeated message: $sameMsgCnt times>", MdColor.GREY tone 400)
+            print("\b\r  $colored")
+        } else {
+            if (sameMsgCnt > 0) {
+                println()
             }
-
-            val isVkMsg = "VkValidation" in (tag ?: "")
-            val printMsg = "$tagStr $messageStr"
-            val checkMsg = if (isVkMsg) printMsg.replace(numberRegex) { "" } else printMsg
-            if (checkMsg == prevMsg) {
-                sameMsgCnt++
-                val colored = coloredText("<Repeated message: $sameMsgCnt times>", MdColor.GREY tone 400)
-                print("\b\r  $colored")
-            } else {
-                if (sameMsgCnt > 0) {
-                    println()
-                }
-                sameMsgCnt = 0
-                prevMsg = checkMsg
-                println("$timestamp|$frame $printMsg")
-            }
+            sameMsgCnt = 0
+            prevMsg = checkMsg
+            println("$timestamp|$frame $printMsg")
         }
     }
 
     private fun coloredText(text: String, fgColor: Color, bgColor: Color? = null, bold: Boolean = false): String {
-        val bg = if (bgColor == null) "" else {
+        val ansiTokens = mutableListOf<String>()
+        if (bgColor != null) {
             val r = (bgColor.r.clamp() * 255).toInt()
             val g = (bgColor.g.clamp() * 255).toInt()
             val b = (bgColor.b.clamp() * 255).toInt()
-            "\u001b[48;2;$r;$g;${b}m"
+            ansiTokens += "48;2;$r;$g;$b"
         }
 
         val r = (fgColor.r.clamp() * 255).toInt()
         val g = (fgColor.g.clamp() * 255).toInt()
         val b = (fgColor.b.clamp() * 255).toInt()
+        ansiTokens += "38;2;$r;$g;$b"
 
-        val weight = if (bold) "\u001b[1m" else ""
-        return "\u001b[38;2;$r;$g;${b}m$weight$bg$text\u001b[0m"
+        if (bold) {
+            ansiTokens += "1"
+        }
+        val style = ansiTokens.joinToString(";")
+        return "\u001b[${style}m$text\u001b[0m"
     }
 
     private data class LevelStyle(val fgColor: Color, val bgColor: Color, val bold: Boolean)
