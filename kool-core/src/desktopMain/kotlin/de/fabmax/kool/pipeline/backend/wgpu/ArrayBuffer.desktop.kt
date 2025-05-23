7814152cd98391862ea575b977cfc008ce21b220
ygdrasil-io <alexandre.mommers@gmail.com>
2025-05-24 01:15:18 +0200

    Add platform-specific implementations for Float32Buffer conversion
    Introduced `asArrayBuffer` for `Float32Buffer` with platform-specific implementations on Android, desktop, and JS. Made modifications for WebGPU-related types and methods to align better with updated APIs. Also refactored storage texture and buffer handling to improve type safety and ensure compatibility.

---
+++ kool-core/src/desktopMain/kotlin/de/fabmax/kool/pipeline/backend/wgpu/ArrayBuffer.desktop.kt
@@ -0,0 +1,7 @@
+package de.fabmax.kool.pipeline.backend.wgpu
+
+import de.fabmax.kool.util.Float32Buffer
+import de.fabmax.kool.util.Float32BufferImpl
+import io.ygdrasil.webgpu.ArrayBuffer
+
+actual fun Float32Buffer.asArrayBuffer(): ArrayBuffer = TODO("Not yet implemented on desktop")
