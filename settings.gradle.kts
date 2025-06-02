4ed98295c7d965bdc59f0422bef5cfca2aea1fdd
ygdrasil-io <alexandre.mommers@gmail.com>
2025-06-02 15:46:50 +0200

    Remove snapshot Maven repository from settings.gradle.kts

--- settings.gradle.kts
+++ settings.gradle.kts
@@ -11,7 +11,6 @@ pluginManagement {
 dependencyResolutionManagement {
     @Suppress("UnstableApiUsage")
     repositories {
-        maven("https://gitlab.com/api/v4/projects/25805863/packages/maven")
         google()
         mavenCentral()
     }
