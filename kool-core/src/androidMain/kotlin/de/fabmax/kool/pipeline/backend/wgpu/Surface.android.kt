c12cefe62333737a24743d01f4e0084847e399d0
ygdrasil-io <alexandre.mommers@gmail.com>
2025-06-02 19:16:56 +0200

    Refactor Surface API to manage GPUTextureView lifecycle
    Introduced `getCurrentTextureView()` for improved texture handling and updated `present()` to properly close and reset resources after presentation.

--- kool-core/src/androidMain/kotlin/de/fabmax/kool/pipeline/backend/wgpu/Surface.android.kt
+++ kool-core/src/androidMain/kotlin/de/fabmax/kool/pipeline/backend/wgpu/Surface.android.kt
@@ -2,6 +2,7 @@ package de.fabmax.kool.pipeline.backend.wgpu
 
 import io.ygdrasil.webgpu.CompositeAlphaMode
 import io.ygdrasil.webgpu.GPUTextureFormat
+import io.ygdrasil.webgpu.GPUTextureView
 import io.ygdrasil.webgpu.NativeSurface
 import io.ygdrasil.webgpu.SurfaceConfiguration
 import io.ygdrasil.webgpu.SurfaceTexture
@@ -15,9 +16,25 @@ actual class WgpuSurface(private val handler: NativeSurface, actual val width: U
     actual val format: GPUTextureFormat
         get() = supportedFormats.first()
 
-    actual fun getCurrentTexture(): SurfaceTexture = handler.getCurrentTexture()
+    private var currentTexture: SurfaceTexture? = null
+    private var currentTextureView: GPUTextureView? = null
 
-    actual fun present() = handler.present()
+    actual fun getCurrentTextureView(): GPUTextureView {
+        if (currentTextureView == null) {
+            currentTexture = handler.getCurrentTexture()
+            currentTextureView = currentTexture?.texture?.createView()
+        }
+
+        return currentTextureView!!
+    }
+
+    actual fun present() {
+        handler.present()
+        currentTextureView?.close()
+        currentTextureView = null
+        currentTexture?.texture?.close()
+        currentTexture = null
+    }
 
     actual fun configure(surfaceConfiguration: SurfaceConfiguration) {
         handler.configure(surfaceConfiguration, width, height)
