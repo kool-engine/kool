a9a7cedc3c8a809ba3d812feb3883ba7a1be2681
fabmax <fabmax.thiele@gmail.com>
2025-06-17 19:33:23 +0200

    removed android sourceset from wgpu4k backend since it was only a placeholder anyway

--- kool-backend-wgpu4k/src/androidMain/kotlin/de/fabmax/kool/pipeline/backend/wgpu/ArrayBuffer.android.kt
+++
@@ -1,9 +0,0 @@
-package de.fabmax.kool.pipeline.backend.wgpu
-
-import de.fabmax.kool.util.Buffer
-import io.ygdrasil.webgpu.ArrayBuffer
-
-actual fun ArrayBuffer.writeInto(target: Buffer): Unit = TODO("Not yet implemented")
-@OptIn(ExperimentalUnsignedTypes::class)
-actual fun ArrayBuffer.asUIntArray(): UIntArray = TODO("Not yet implemented")
-actual fun Buffer.asArrayBuffer(block: (ArrayBuffer) -> Unit): Unit = TODO("Not yet implemented")
