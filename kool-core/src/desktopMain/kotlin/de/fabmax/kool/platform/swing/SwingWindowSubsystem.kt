c908700f867d2e46043d419fd2ecf9ecb8b1f697
Max Thiele <fabmax.thiele@gmail.com>
2025-10-28 22:41:23 +0100

    Add additional pause render checks

--- kool-core/src/desktopMain/kotlin/de/fabmax/kool/platform/swing/SwingWindowSubsystem.kt
+++ kool-core/src/desktopMain/kotlin/de/fabmax/kool/platform/swing/SwingWindowSubsystem.kt
@@ -176,7 +176,7 @@ class SwingWindowSubsystem(
             val window = canvasWrapper!!
             while (!isCloseRequested) {
                 window.pollEvents()
-                if (!window.flags.isMinimized) {
+                if (!window.flags.isMinimized && window.flags.isVisible && window.size.x > 0 && window.size.y > 0) {
                     ctx.renderFrame()
                 } else {
                     Thread.sleep(10)
