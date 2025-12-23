f4cd6e094c56a82d7fd73f0a1d1f0bbde27816a3
Max Thiele <fabmax.thiele@gmail.com>
2025-12-23 16:46:10 +0100

    Add wgpu4k wasm bindings

---
+++ kool-backend-wgpu4k/src/wasmJsMain/kotlin/de/fabmax/kool/pipeline/backend/wgpu/ArrayBuffer.js.kt
@@ -0,0 +1,36 @@
+package de.fabmax.kool.pipeline.backend.wgpu
+
+import de.fabmax.kool.util.*
+import io.ygdrasil.webgpu.ArrayBuffer
+import org.khronos.webgl.*
+
+actual fun ArrayBuffer.writeInto(target: Buffer): Unit = when (target) {
+    is Float32BufferImpl -> target.buffer.set(Float32Array(this))
+    is Int32BufferImpl -> target.buffer.set(Int32Array(this))
+    is MixedBufferImpl -> Uint8Array(target.buffer.buffer).set(Uint8Array(this))
+    is Uint16BufferImpl -> target.buffer.set(Uint16Array(this))
+    is Uint8BufferImpl -> target.buffer.set(Uint8Array(this))
+    else -> error("Unsupported buffer type ${target::class.simpleName}")
+}
+
+private fun Buffer.asArrayBuffer(): ArrayBuffer = when (this) {
+    is Float32BufferImpl -> this.buffer.buffer
+    is Int32BufferImpl -> this.buffer.buffer
+    is MixedBufferImpl ->  this.buffer.buffer
+    is Uint16BufferImpl -> this.buffer.buffer
+    is Uint8BufferImpl -> this.buffer.buffer
+    else -> error("Unsupported buffer type ${this::class.simpleName}")
+}
+
+@OptIn(ExperimentalUnsignedTypes::class)
+actual fun ArrayBuffer.asUIntArray(): UIntArray = Int32Array(this).let {
+    val ktArray = UIntArray(it.length)
+    for (i in 0 until it.length) {
+        ktArray[i] = it[i].toUInt()
+    }
+    ktArray
+}
+
+actual fun Buffer.asArrayBuffer(block: (ArrayBuffer) -> Unit) {
+    block(asArrayBuffer())
+}
